cc426133c06f772ee93afc3c9c58b6ebe51fa822
i.kardanov <i.kardanov@wisetech.pro>
2022-10-10 15:07:31 +0300

    Добавил класс static_thread_pool

--- thread_pool/static_thread_pool/static_thread_pool.cpp
+++ thread_pool/static_thread_pool/static_thread_pool.cpp
@@ -1,3 +1,62 @@
-//
-// Created by islam on 2022-10-10.
-//
+#include "static_thread_pool.h"
+
+#include <algorithm>
+#include <stdexcept>
+
+namespace thread {
+
+StaticThreadPool::StaticThreadPool(const unsigned short threadsCount):
+workers_(threadsCount),
+queue_(),
+queueEventDriver_(),
+queueLock_() {
+    std::generate_n(workers_.begin(), threadsCount, [this]() {
+        return std::thread([this]() {
+            work();
+        });
+    });
+}
+
+void StaticThreadPool::submitTask(const StaticThreadPool::Task &task) {
+    std::lock_guard lock(queueLock_);
+    try {
+        queue_.push(task);
+    } catch (const std::bad_alloc &e) {
+        throw std::runtime_error("Can`t submit a new task to static thread pool. "
+                                 "Resource limit exceeded");
+    }
+    queueEventDriver_.notify_one();
+}
+
+void StaticThreadPool::join() {
+    for (auto i = 0; i < workers_.size(); ++i) {
+        submitTask({});
+    }
+
+    std::for_each_n(workers_.begin(), workers_.size(), [](auto &worker){
+        if (worker.joinable()) {
+            worker.join();
+        }
+    });
+}
+
+void StaticThreadPool::work() {
+    while (true) {
+        std::unique_lock lock(queueLock_);
+        while (queue_.empty()) {
+            queueEventDriver_.wait(lock);
+        }
+
+        auto task = queue_.back();
+        queue_.pop();
+        lock.unlock();
+
+        if (!task) {
+            break;
+        } else {
+            task();
+        }
+    }
+}
+
+} // namespace thread
--- thread_pool/static_thread_pool/static_thread_pool.h
+++ thread_pool/static_thread_pool/static_thread_pool.h
@@ -1,8 +1,38 @@
-//
-// Created by islam on 2022-10-10.
-//
-
 #ifndef ASYNCIO_STATIC_THREAD_POOL_H
 #define ASYNCIO_STATIC_THREAD_POOL_H
+
+#include <vector>
+#include <queue>
+#include <condition_variable>
+#include <thread>
+#include <mutex>
+#include <functional>
+
+namespace thread {
+
+class StaticThreadPool final {
+public:
+    using Workers = std::vector<std::thread>;
+    using Task = std::function<void()>;
+    using TaskQueue = std::queue<Task>;
+
+    explicit StaticThreadPool(unsigned short threadsCount = 1U);
+    StaticThreadPool(StaticThreadPool &&other) = default;
+    StaticThreadPool &operator=(StaticThreadPool &&other) = default;
+    StaticThreadPool(const StaticThreadPool &other) = delete;
+    StaticThreadPool &operator=(const StaticThreadPool &other) = delete;
+    void submitTask(const Task &task);
+    void join();
+
+private:
+    void work();
+
+    Workers workers_;
+    TaskQueue queue_;
+    std::condition_variable queueEventDriver_;
+    std::mutex queueLock_;
+};
+
+} // namespace thread
 
 #endif //ASYNCIO_STATIC_THREAD_POOL_H
